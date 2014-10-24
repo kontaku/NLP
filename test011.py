7e27ce0b188e240a65e276adea82371e3a50e6a8
kontaku <kondo-takuya@ed.tmu.ac.jp>
2014-10-24 17:32:54 +0900

    debug first set

---
+++ test011.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import sys
+
+if __name__=='__main__':
+	print u'あ'
+	
+	#print chardet.detect(a)
+	#for line in sys.stdin.readlines():
+	#	print line
+		
